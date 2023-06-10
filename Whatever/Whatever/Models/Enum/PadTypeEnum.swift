2d2c18eabe500dac068a6af3de47bd6f50036e37
sei <vanism7557@gmail.com>
2023-06-11 01:50:20 +0900

    [Add] CoreData 관련 Enum type 정의

--- Whatever/Whatever/Models/Enum/PadTypeEnum.swift
+++ Whatever/Whatever/Models/Enum/PadTypeEnum.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum PadTypeEnum: Int, CaseIterable, Identifiable {
+    case liner = 0, small, mid, big, over
+    
+    var description: String {
+        switch self {
+        case .liner: return "팬라"
+        case .small: return "소형"
+        case .mid: return "중형"
+        case .big: return "대형"
+        case .over: return "오버"
+        }
+    }
+    var id: Self { self }
+}
