300da2fe42d21a836789eee770919b1a8ddefbd6
금가경 <gageum0@gmail.com>
2023-06-12 21:09:18 +0900

    [Setting] 컬러 에셋 셋팅

---
+++ Whatever/Whatever/Resources/Color+.swift
@@ -0,0 +1,18 @@
+//
+//  Color+.swift
+//  Whatever
+//
+//  Created by 금가경 on 2023/06/12.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundPurple = Color("backgroundPurple")
+    static let clearBlack = Color("clearBlack")
+    static let clearWhite = Color("clearWhite")
+    static let lightPurple = Color("lightPurple")
+    static let mediumGray = Color("mediumGray")
+    static let vividPurple = Color("vividPurple")
+    static let vividYellow = Color("vividYellow")
+}
