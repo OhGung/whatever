b6a892caa548426ab439ac0acc1abfa7553c24f8
chaekie <chaekie@chaekieui-noteubug.local>
2023-06-13 22:30:18 +0900

    현재달 기준 전후 달 추가하여 보여주기 구현

--- Whatever/Whatever/Views/CalenderView.swift
+++ Whatever/Whatever/Views/CalenderView.swift
@@ -8,32 +8,42 @@
 import SwiftUI
 
 struct CalenderView: View {
+    @State private var months: [Int] = []
     @State var selectedMonth = 0
     @State var selectedDay = 0
+    var currentMonth = Calendar.current.component(.month, from: Date())
+    let loadingCount = 5
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            LazyVStack(alignment: .leading) {
-                ForEach(6...11, id: \.self) { month in
-                    @State var month = month
-                    MonthlyView(month: $month,
-                             selectedDay: $selectedDay,
-                             selectedMonth: $selectedMonth)
+        ScrollViewReader { scrollView in
+            ScrollView(showsIndicators: false) {
+                LazyVStack(alignment: .leading) {
+                    ForEach((currentMonth - loadingCount)...(currentMonth + loadingCount),
+                            id: \.self) { month in
+                        MonthlyView(month: month,
+                                    selectedDay: $selectedDay,
+                                    selectedMonth: $selectedMonth)
+                    }
                 }
             }
+            .clipped()
+            .onAppear {
+                scrollView.scrollTo(currentMonth, anchor: .top)
+            }
         }
-        .clipped()
     }
     
     struct MonthlyView: View {
-        @Binding var month: Int
+        var month: Int
         @Binding var selectedDay: Int
         @Binding var selectedMonth: Int
-        @StateObject var customCalenderVM =  CalendarViewModel()
+        @StateObject var customCalenderVM = CalendarViewModel()
         @State var dayInMonth = 30
         @State var startingDay = 0
         @State var isCurrentMonth = false
-        
+        @State var yearGap = 0
+        @State private var newMonth = 0
+
         let columns = [
             GridItem(.flexible()),
             GridItem(.flexible()),
@@ -43,10 +53,10 @@ struct CalenderView: View {
             GridItem(.flexible()),
             GridItem(.flexible()),
         ]
-                
+        
         var body: some View {
             VStack {
-                Text("\(String(customCalenderVM.currentYear))년 \(month)월")
+                Text("\(String(customCalenderVM.currentYear + yearGap))년 \(newMonth)월")
                     .bold()
                     .foregroundColor(isCurrentMonth ? Color.black : Color.gray)
                     .frame(maxWidth: .infinity)
@@ -55,7 +65,7 @@ struct CalenderView: View {
                 CalenderDayView(isCurrentMonth: $isCurrentMonth)
                 
                 LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(1...(dayInMonth+startingDay-1), id: \.self) { item in
+                    ForEach(1...(dayInMonth + startingDay - 1), id: \.self) { item in
                         if item < startingDay {
                             Text("")
                         }
@@ -73,9 +83,11 @@ struct CalenderView: View {
                 }
                 .padding()
             }.onAppear {
-                findDaysInMonth(month)
-                isCurrentMonth = checkIsCurrentMonth(month)
+                calculateNewMonth(month)
+                calculateYearGap(month)
+                isCurrentMonth = month == customCalenderVM.currentMonth
                 selectedMonth = customCalenderVM.currentMonth
+                findDaysInMonth(month)
             }
         }
         
@@ -86,10 +98,7 @@ struct CalenderView: View {
             startingDay = customCalenderVM.findWeekdayName(date: someMonth!)
             let interval = calendar.dateInterval(of: .month, for: someMonth!)!
             self.dayInMonth = calendar.dateComponents([.day], from: interval.start, to: interval.end).day!
-        }
-        
-        func checkIsCurrentMonth(_ month: Int) -> Bool {
-            return month == customCalenderVM.currentMonth ? true : false
+            
         }
         
         func getFocusedDayColor(_ state: String) -> Color {
@@ -113,6 +122,24 @@ struct CalenderView: View {
                     selectedDay = dayInt
                     selectedMonth = month
                 }
+        }
+        
+        func calculateNewMonth(_ month: Int) {
+            if month > 12 {
+                newMonth = month % 12
+            } else if month < 1 {
+                newMonth = 12 + month % 12
+            } else {
+                newMonth = month
+            }
+        }
+        
+        func calculateYearGap(_ month: Int) {
+            if month < 0 {
+                yearGap = month / 12 - 1
+            } else {
+                yearGap = month / 12
+            }
         }
     }
 }
