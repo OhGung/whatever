c733dfc5f09d39b43fc13f9eca25c6256e72ab5d
금가경 <gageum0@gmail.com>
2023-06-14 16:39:08 +0900

    [Feat] 알림 보내기 기능 구현

--- Whatever/Whatever/Models/LocalNotificationHelper.swift
+++ Whatever/Whatever/Models/LocalNotificationHelper.swift
@@ -13,11 +13,48 @@ class LocalNotificationHelper {
     
     private init() { }
     
-    func setAuthorization() {
+    func setAuthorization(completion: ((_ success: Bool) -> Void)? = nil) {
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
         UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
-            completionHandler: { _, _ in }
+            completionHandler: { success, error in
+                if let error = error {
+                    print(#function, error)
+                }
+                
+                if let completion = completion {
+                    completion(success)
+                }
+            }
         )
+    }
+    func pushNotification(title: String, body: String, seconds: TimeInterval, identifier: String) {
+        // 알림 내용, 설정
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = title
+        notificationContent.body = body
+
+        // 조건(시간, 반복)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: false)
+        
+        // 요청
+        let request = UNNotificationRequest(identifier: identifier,
+                                            content: notificationContent,
+                                            trigger: trigger)
+
+        // 알림 등록
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Notification Error: ", error)
+            }
+        }
+        
+        //알림 등록 되었는지 확인
+        UNUserNotificationCenter.current().getPendingNotificationRequests { requests in
+            print("===== Pending =====")
+            for request in requests {
+                print(request)
+            }
+        }
     }
 }
--- Whatever/Whatever/Views/SettingAlarmView.swift
+++ Whatever/Whatever/Views/SettingAlarmView.swift
@@ -15,6 +15,12 @@ struct SettingAlarmView: View {
             HStack {
                 Text("알림 허용")
                 Toggle(isOn: $isAlarmOn) {
+                    Text("")
+                }
+                .onChange(of: isAlarmOn) { newState in
+                    if newState {
+                        LocalNotificationHelper.shared.pushNotification(title: "오늘은 생리 3일차", body: "소형 생리대 3개를 준비해 4시간마다 갈아줘!", seconds: 5, identifier: "PUSH_TEST")
+                    }
                 }
             }
         }
